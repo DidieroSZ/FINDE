241d2aa5b1563dac4d3eaf28c3b9623d1ddef8cb
DidieroSZ <octaviosz2003@hotmail.com>
2026-05-21 16:14:42 -0600

    feat(js): Added Loading Animation - 21/05/2026

--- src/views/display-view.js
+++ src/views/display-view.js
@@ -33,35 +33,50 @@ export class DisplayView extends LitElement {
     render() {
         return html`
             <section class="general-section display-container">
-                <aside class="display-sections">
+                <aside class="display-sections filters-container">
+                    <ul class="filters-list">
 
-                <div class="dropdown">
-                    <input hidden="" class="sr-only" name="state-dropdown" id="state-dropdown" type="checkbox"/>
-                    <label aria-label="dropdown scrollbar" for="state-dropdown" class="trigger"></label>
-                    <ul class="list webkit-scrollbar" role="list" dir="auto">
-                        <li class="listitem" role="listitem">
-                            <article class="article">Hover to view scrollbar.</article>
+                        <li class="filter-display trans">
+                            <details class="filter-details">
+                                <summary class="filter-summary d-flexx d-row">
+                                    <span class="filter-icon d-flexx">${unsafeHTML(icons.pin)}</span>
+                                    <p>Ubicación</p>
+                                    <span class="filter-visual d-flexx trans">${unsafeHTML(icons.chevronDown)}</span>
+                                </summary>
+                            </details>
+                            <div class="filter-body trans">
+                                <div class="inner-content">
+                                    - Cerca de mí
+                                    - Distancia (1km, 5km, 10km…)
+                                    - Colonia / zona
+                                    - En mapa
+                                </div>
+                            </div>
                         </li>
-                        <li class="listitem" role="listitem">
-                            <article class="article">
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium,
-                                sunt tempora recusandae dolorum.
-                            </article>
+                        <li class="filter-display trans">
+                            <details class="filter-details">
+                                <summary class="filter-summary d-flexx d-row">
+                                    <span class="filter-icon d-flexx">${unsafeHTML(icons.dollar)}</span>
+                                    <p>Precio</p>
+                                    <span class="filter-visual d-flexx trans">${unsafeHTML(icons.chevronDown)}</span>
+                                </summary>
+                            </details>
+                            <div class="filter-body trans">
+                                <div class="inner-content">
+                                    - Cerca de mí
+                                    - Distancia (1km, 5km, 10km…)
+                                    - Colonia / zona
+                                    - En mapa
+                                </div>
+                            </div>
                         </li>
-                        <li class="listitem" role="listitem">
-                            <article class="article">
-                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium,
-                                sunt tempora recusandae dolorum.
-                            </article>
-                        </li>
+
                     </ul>
-                </div>
-
-                
-                
-                
                 </aside>
-                <article class="display-sections">RENDER</article>
+
+                <article class="display-sections">
+                    RENDER
+                </article>
             </section>
         `;
     }
