e2aa7671c58283c639ba00daddcb21ff2038f792
DidieroSZ <octaviosz2003@hotmail.com>
2026-05-21 10:05:45 -0600

    feat(js):  Added filters - 21/05/2026

--- src/views/display-view.js
+++ src/views/display-view.js
@@ -33,7 +33,34 @@ export class DisplayView extends LitElement {
     render() {
         return html`
             <section class="general-section display-container">
-                <aside class="display-sections">FILTROS</aside>
+                <aside class="display-sections">
+
+                <div class="dropdown">
+                    <input hidden="" class="sr-only" name="state-dropdown" id="state-dropdown" type="checkbox"/>
+                    <label aria-label="dropdown scrollbar" for="state-dropdown" class="trigger"></label>
+                    <ul class="list webkit-scrollbar" role="list" dir="auto">
+                        <li class="listitem" role="listitem">
+                            <article class="article">Hover to view scrollbar.</article>
+                        </li>
+                        <li class="listitem" role="listitem">
+                            <article class="article">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium,
+                                sunt tempora recusandae dolorum.
+                            </article>
+                        </li>
+                        <li class="listitem" role="listitem">
+                            <article class="article">
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium,
+                                sunt tempora recusandae dolorum.
+                            </article>
+                        </li>
+                    </ul>
+                </div>
+
+                
+                
+                
+                </aside>
                 <article class="display-sections">RENDER</article>
             </section>
         `;
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
-    base: '/PASSWORD-GENERATOR/',
+    base: '/FINDE/',
     build: {
         outDir: 'dist',
     },
